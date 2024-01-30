44d7bc9037f08167b495ca3090c3dfc732567326
MatrixMe <safosah00@gmail.com>
2024-01-30 13:44:33 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view("auth.login", [
+        "page_title" => "Login"
+    ]);
 });
+
+Route::get("/setup", function(){
+    return view("auth.register", [
+        "role_id" => 2,
+        "page_title" => "Activate System"
+    ]);
+})->name('setup');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
