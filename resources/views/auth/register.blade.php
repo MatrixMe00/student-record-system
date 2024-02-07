eb17cfdc0afa13915a2399f0cbd97467ef38fc0a
MatrixMe <safosah00@gmail.com>
2024-02-07 13:36:52 +0000

    Added school registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,24 @@
 <x-guest-layout>
     @section('title', $page_title)
     <!-- component -->
-    @switch($role_id)
-        @case(1)
-        @case(2)
-            @include("auth.partials._superadmin")
+    @if (isset($role_id))
+        @switch($role_id)
+            @case(1)
+            @case(2)
+                @include("auth.partials._superadmin")
+                @break
+            @case(3)
+                @include("auth.partials._admin")
             @break
-        @case(3)
-            @include("auth.partials._admin")
-        @break
 
-        @default
-            {{ "No form to deliver" }}
-    @endswitch
+            @default
+                {{ "No form to deliver" }}
+        @endswitch
+
+    @elseif (isset($admin_id))
+        @include("auth.partials._school")
+    @else
+        {{ "There is nothing to show" }}
+    @endif
+
 </x-guest-layout>
--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,4 +1,4 @@
-@props(["options", "default", "value_key" => "id", "text_key" => "name"])
+@props(["options", "default" => "", "value_key" => "id", "text_key" => "name"])
 
 <select
 {!! $attributes->merge(['class' => 'w-full mt-2 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}
@@ -8,6 +8,8 @@
     @foreach ($options as $option)
         <option value="{{ $option[$value_key] }}">{{ ucfirst($option[$text_key]) }}</option>
     @endforeach
+@elseif ($options == 0)
+    {{ $slot }}
 @else
     <option value="">No Options Data</option>
 @endif
