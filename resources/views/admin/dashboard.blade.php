fdca2497d46e83b1a6bc482d29d7e42d2f26f52e
MatrixMe <safosah00@gmail.com>
2024-02-20 10:27:00 +0000

    Update dashboard.blade.php
    Created a dashboard for the admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,10 @@
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-            {{ __("You're logged in!") }}
-        </div>
+<x-app-main>
+    <div class="bg-transparent grid grid-cols-1 sm:grid-cols-2
+        md:grid-cols-3 lg:grid-cols-4 dark:bg-gray-800 overflow-hidden
+        shadow-sm sm:rounded-lg py-4 gap-4">
+        <x-dashboard-card icon="fas fa-user-tie" context="{{ $admin_count }}" title="Admins" />
+        <x-dashboard-card icon="fas fa-chalkboard-user" context="{{ $teacher_count }}" title="Teachers" />
+        <x-dashboard-card icon="fas fa-user-graduate" context="{{ $student_count }}" title="Students" />
+        <x-dashboard-card icon="fas fa-user-minus" context="{{ $delete_count }}" title="Deleted Users" />
     </div>
-</div>
+</x-app-main>
