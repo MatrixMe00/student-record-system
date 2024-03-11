d2d7be9ea26284f98d4c9eb99f64250633f2e256
MatrixMe <safosah00@gmail.com>
2024-03-11 08:03:36 +0000

    Added tables to handle remarks

---
+++ database/migrations/2024_03_10_205507_create_teacher_remarks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_remarks', function (Blueprint $table) {
+            $table->id();
+            $table->string("remark_token");
+            $table->foreignId("school_id")->constrained()->cascadeOnDelete();
+            $table->foreignId("teacher_id")->constrained("teachers", "user_id");
+            $table->foreignId("program_id")->constrained()->cascadeOnDelete();
+            $table->foreignId("student_id")->constrained("students", "user_id");
+            $table->integer("semester");
+            $table->integer("total_marks");
+            $table->integer("position");
+            $table->string("remark");
+            $table->enum("status", ["pending", "accepted", "rejected"])->default("pending");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_remarks');
+    }
+};
---
+++ database/migrations/2024_03_10_222024_create_teachers_remarks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers_remarks', function (Blueprint $table) {
+            $table->id();
+            $table->string("remark_token");
+            $table->foreignId("school_id")->constrained();
+            $table->foreignId("teacher_id")->constrained("teachers", "user_id");
+            $table->foreignId("program_id")->constrained();
+            $table->integer("semester");
+            $table->enum("status", ["pending", "rejected", "accepted"])->default("pending");
+            $table->foreignId("admin_id")->constrained("admins", "user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers_remarks');
+    }
+};
