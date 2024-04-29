8ee4e446f3c0ed26735c1b5da6fc645dceff5026
MatrixMe <safosah00@gmail.com>
2024-04-29 22:50:43 +0000

    Update _edit_remark_body.blade.php

--- resources/views/remarks/partials/_edit_remark_body.blade.php
+++ resources/views/remarks/partials/_edit_remark_body.blade.php
@@ -2,44 +2,38 @@
     {{ __($program->name.' result summary list [Term '.$remark_head->semester.']') }}
 </h1>
 
-@if($remarks->count() > 0 && auth()->user()->role_id === 3)
-    <x-form-element mdcols="" action="{{ route('remarks.store') }}" method="POST">
-        @csrf
+<x-form-element mdcols="" action="{{ route('remarks.store') }}" method="POST">
+    @csrf
 
-        @php
-            $show_submit = $remarks->count() > 0 || $student_marks->count() > 0;
-        @endphp
+    @php
+        $show_submit = $remarks->count() > 0 || $student_marks->count() > 0;
+    @endphp
 
-        @if ($errors->any())
-            <x-input-error :messages="$errors->all()" />
-        @endif
+    @if ($errors->any())
+        <x-input-error :messages="$errors->all()" />
+    @endif
 
-        {{-- default items --}}
-        @include('remarks.partials._remark_body_defaults')
+    {{-- default items --}}
+    @include('remarks.partials._remark_body_defaults')
 
-        @if (!$show_submit)
-            <p class="p-4 text-center">
-                {{ __("No Student data was found for this class") }}
-            </p>
-        @elseif ($remarks->count() > 0)
-            {{-- table with grades --}}
-            @include('remarks.partials._remark_body_grades')
-        @else
-            {{-- table with new student data --}}
-            @include('remarks.partials._new_remark_body')
-        @endif
+    @if (!$show_submit)
+        <p class="p-4 text-center">
+            {{ __("No Student data was found for this class") }}
+        </p>
+    @elseif ($remarks->count() > 0)
+        {{-- table with grades --}}
+        @include('remarks.partials._remark_body_grades')
+    @else
+        {{-- table with new student data --}}
+        @include('remarks.partials._new_remark_body')
+    @endif
 
-        @if ($show_submit && !$is_admin)
-            {{-- submissions for teacher --}}
-            @include("results.partials._teacher_buttons")
+    @if ($show_submit && !$is_admin)
+        {{-- submissions for teacher --}}
+        @include("results.partials._teacher_buttons")
 
-        @elseif ($is_admin)
-            {{-- submission buttons for admin --}}
-            @include("results.partials._admin_buttons")
-        @endif
-    </x-form-element>
-@else
-    <x-empty-div>
-        No Remarks Identified
-    </x-empty-div>
-@endif
+    @elseif ($is_admin)
+        {{-- submission buttons for admin --}}
+        @include("results.partials._admin_buttons")
+    @endif
+</x-form-element>
